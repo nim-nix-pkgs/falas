4a1ab490575a5f0b725b777ab6b46cdde48a5469
hugosenari <hugosenari@gmail.com>
2022-03-27 21:49:27 -0300

    chore: re index falas

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-falas-master.ref   = "refs/heads/master";
   inputs.src-falas-master.owner = "brentp";
   inputs.src-falas-master.repo  = "falas";
-  inputs.src-falas-master.dir   = "";
   inputs.src-falas-master.type  = "github";
   
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
